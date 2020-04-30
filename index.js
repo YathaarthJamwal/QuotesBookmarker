05fcdb2f283bdd0f1592c2bbbf50b13e4713468b
YathaarthJamwal <yathaarth.jamwal@gmail.com>
2020-05-01 01:35:15 +0530

    new 1

--- index.js
+++ index.js
@@ -82,7 +82,8 @@ app.get('/api/quotes', cors(), async(req, res, next) => {
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    // res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    console.log("Error Logged");
 });
 
 const port = process.env.PORT || 5000;
