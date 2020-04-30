d62f72a7419d5af5975d8f56197db2d614410301
YathaarthJamwal <yathaarth.jamwal@gmail.com>
2020-04-30 18:18:22 +0530

    Deploy try again

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const cors = require('cors');
 
 const scraper = () => {
     //let quotesData = null;
@@ -49,19 +50,40 @@ console.log(quotesData);
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Put all API endpoints under '/api'
-app.get('/api/quotes', (req, res) => {
+app.get('/api/quotes', cors(), async(req, res, next) => {
     // Return them as json
-    res.json(quotesData);
-    console.log(quotesData);
-
-    console.log(`Sent ${quotesData.length} quotes`);
+    try {
+        res.json(quotesData);
+        console.log(quotesData);
+        console.log(`Sent ${quotesData.length} quotes`);
+    } catch (err) {
+        next(err);
+    }
 });
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
+// app.get('/api/cow/', cors(), async(req, res, next) => {
+//     try {
+//         const moo = cowsay.say({ text: 'Hello World!' })
+//         res.json({ moo })
+//     } catch (err) {
+//         next(err)
+//     }
+// })
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
+
+// // The "catchall" handler: for any request that doesn't
+// // match one above, send back React's index.html file.
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/client/build/index.html'));
+// });
 
 const port = process.env.PORT || 5000;
 app.listen(port);
